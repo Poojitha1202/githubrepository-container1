2bb643cca41ca7333fded88538c33d72d81ab16c
Poojitha Mummadi <HP@sais-air.home>
2024-07-10 20:13:04 -0300

    Cloud build push service account 1

--- container1.py
+++ container1.py
@@ -1,49 +1,51 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
-# Directory to store files, mounted to a Kubernetes Persistent Volume
-FILE_DIRECTORY = "/path_to_your_persistent_volume"
-CONTAINER_2_ENDPOINT_VALIDATE_CSV = 'http://container2:4000/validate-csv'
-CONTAINER_2_ENDPOINT_SUM_PROCESS = 'http://container2:4000/processSum'
+CONTAINER_2_ENDPOINT_SUM = 'http://localhost:4000/processSum'
+STORAGE_PATH = "../poojitha_PV_dir"  
 
 @app.route('/store-file', methods=['POST'])
 def store_file():
     data = request.get_json()
-    if not data or 'file' not in data or 'data' not in data:
+
+    if 'file' not in data or not data['file']:
         return jsonify({"file": None, "error": "Invalid JSON input."}), 400
-    
-    filename = data['file']
-    content = data['data']
-    filepath = os.path.join(FILE_DIRECTORY, filename)
-    
+
+    file_content = data.get('data', '').replace(' ', '')
+
+    lines = file_content.split('\n')
+
+    if lines:
+        lines[0] = lines[0].replace(',', ', ')
+
+    formatted_content = '\n'.join(lines)
+
     try:
-        with open(filepath, 'w') as file:
-            file.write(content)
-        return jsonify({"file": filename, "message": "Success."}), 200
+        os.makedirs(STORAGE_PATH, exist_ok=True)
+        file_path = os.path.join(STORAGE_PATH, data['file'])  
+        with open(file_path, 'w') as file:
+            file.write(formatted_content)
+        return jsonify({"file": data['file'], "message": "Success."})
     except Exception as e:
-        return jsonify({"file": filename, "error": f"Error while storing the file to the storage: {str(e)}"}), 500
+        return jsonify({"file": data['file'], "error": f"Error while storing the file to the storage: {e}"}), 500
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    input_data = request.get_json()
+    data = request.get_json()
     
-   
-    if 'file' not in input_data or not input_data['file']:
+    if 'file' not in data or not data['file']:
         return jsonify({"file": None, "error": "Invalid JSON input."}), 400
     
+    file_path = os.path.join(STORAGE_PATH, data['file'])
+    if not os.path.exists(file_path):
+        return jsonify({"file": data["file"], "error": "File not found."}), 404
+
     try:
-        response = requests.post(CONTAINER_2_ENDPOINT_CHECK_FILE_EXISTS, json=input_data)
-        check_file_exists = response.json().get('exists', False)
-        if not check_file_exists:
-            return jsonify({"file": input_data["file"], "error": "File not found."}), 404
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
-    
-    try:
-        sum_response = requests.post(CONTAINER_2_ENDPOINT_SUM_PROCESS, json=input_data)
-        return sum_response.json(), sum_response.status_code
+        response = requests.post(CONTAINER_2_ENDPOINT_SUM, json=data)
+        return response.json(), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
